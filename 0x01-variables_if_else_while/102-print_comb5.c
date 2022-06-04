11a77890ca2825e839e83672a6419ea125f6af27
Cheediogo <bkinetix@gmail.com>
2022-06-03 20:52:13 -0700

    variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch, n;
+
+
+	for (ch = 0; ch <= 98; ch++)
+	{
+		for (n = ch + 1; n <= 99; n++)
+		{
+			putchar((ch / 10) + '0');
+			putchar((ch % 10) + '0');
+			putchar(' ');
+			putchar((n / 10) + '0' );
+			putchar ((n % 10) + '0');
+
+			if (ch == 98 && n == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
