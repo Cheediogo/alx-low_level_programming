6090157a429f45af8cb7bd96aa875e644cb5befa
Cheediogo <bkinetix@gmail.com>
2022-06-03 20:40:00 -0700

    variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,28 +8,31 @@
 
 int main(void)
 {
-	int ch;
+	int ch, n, m;
 
 
 	for (ch = 48; ch < 58; ch++)
 	{
 		for (n = 48; n < 58; n++)
 		{
-			if (ch == n || ch == m || n == m || n <= ch || m <= n)
+			for (m = 48; m < 58; m++)
 			{
-			}
-			else
-			{
-				putchar(ch);
-				putchar(n);
-				putchar(m);
-				if (ch == 55 && n == 56 && m == 57)
+				if (ch == n || ch == m || n == m || n <= ch || m <= n)
 				{
 				}
 				else
 				{
-					putchar(44);
-					putchar(' ');
+					putchar(ch);
+					putchar(n);
+					putchar(m);
+					if (ch == 55 && n == 56 && m == 57)
+					{
+					}
+					else
+					{
+						putchar(44);
+						putchar(' ');
+					}
 				}
 			}
 		}
