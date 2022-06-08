6b739473812a1efa0ca24e6e3889cea35e3b47be
Cheediogo <bkinetix@gmail.com>
2022-06-08 01:31:45 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - function prints _putchar
+ * A program that prints _putchar, followed by a new line.
+ * Return: 0
+*/
+
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n);
+	return(0);
+}
