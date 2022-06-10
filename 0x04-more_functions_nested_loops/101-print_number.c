7320ed39b6a69b634d5dcc0356b56f3c50ecd94f
Cheediogo <bkinetix@gmail.com>
2022-06-09 17:16:18 -0700

    nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,17 @@
  * Return:void
  */
 
-void print_number(int n)
-{
-	unsigned int y;
+ void print_number(int n)
+ {
+	 unsigned int y;
 
-	y = n;
-	if (n < 0)
-	{
-	 _putchar(45);
-	 y = -n;
+	 y = n;
+	 if (n < 0)
+	 {
+		 _putchar(45);
+		 y = -n;
 	 }
 	 if (y / 10)
-	 	print_number(y / 10);
-	_putchar((y % 10) + '0');
-}
+		 print_number(y / 10);
+	 _putchar((y % 10) + '0');
+ }
