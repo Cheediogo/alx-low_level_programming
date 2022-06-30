031afe8485cd56557df42e0313c1690c5146f2cd
Cheediogo <bkinetix@gmail.com>
2022-06-30 15:41:12 -0700

    malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,14 +29,13 @@ void _print(char *str, int l)
 
 **
 *mul - multiplies a char with a string and places the answer into dest
-*@n : char to multiply
-*@num : string to multiply
-*@num_index : last non NULL index of num
-*@dest : destination of multiplication
-*@dest_index : highest index to start addition
-*
-*Return : pointer to dest, or NULL on failure
-*/
+* @n : char to multiply
+* @num : string to multiply
+* @num_index : last non NULL index of num
+* @dest : destination of multiplication
+* @dest_index : highest index to start addition
+* Return : pointer to dest, or NULL on failure
+* /
 
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
