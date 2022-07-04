10da6cd9021e35fc5c7702cd89d62e38e8b6bd06
Cheediogo <bkinetix@gmail.com>
2022-07-04 13:01:00 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - create a dog structure
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
